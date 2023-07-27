54c7ae1b0da47d4dfc65f9d2e80f842c50942bdc
“jayfang” <1317618988@qq.com>
2023-07-27 09:40:49 +0800

    添加性能监视插件

--- src/ts/TEngine.ts
+++ src/ts/TEngine.ts
@@ -1,5 +1,6 @@
-import { AmbientLight, AxesHelper, BoxGeometry, GridHelper, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from "three";
+import { AmbientLight, AmbientLight, AxesHelper, BoxGeometry, GridHelper, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 export class TEngine{
 
     private dom: HTMLElement;
@@ -12,7 +13,9 @@ export class TEngine{
         const width = this.dom.offsetWidth;
         const height = this.dom.offsetHeight;
 
-        this.renderer = new WebGLRenderer();
+        this.renderer = new WebGLRenderer({
+            antialias: true, // 设置抗锯齿
+        });
         this.scene = new Scene();
         this.camera = new PerspectiveCamera(30, width/height, 1, 1000);
 
@@ -25,6 +28,9 @@ export class TEngine{
         const gridHelper = new GridHelper(1000, 20, 'rgb(100, 100, 100)', 'rgb(150, 150, 150)');
         gridHelper.position.set(0, -0.1, 0)
         this.scene.add(gridHelper);
+
+        const ambientLight = new AmbientLight('rgb(255, 255, 255)', 1);
+        this.scene.add(ambientLight);
 
         // 渲染基本图形
         const box = new BoxGeometry(50 ,50 ,50);
@@ -41,15 +47,27 @@ export class TEngine{
         this.renderer.setClearColor('rgb(125, 125, 125)', 0.5);
         this.renderer.clearColor();
 
-        // 设置相机控件
+        // 设置性能监视器
+        const stats = Stats(); 
+        const statsDom = stats.domElement;
+        statsDom.style.position = 'fixed';
+        statsDom.style.top = '10px';
+        statsDom.style.left = '10px';
+
+        dom.appendChild(statsDom);
+
+        // 设置相机控件：轨道控制器
         const oribitControls = new OrbitControls(this.camera, this.renderer.domElement);
         // oribitControls.autoRotate = true;
 
         const looper = () => {
-            requestAnimationFrame(looper);
             mesh.rotateY(0.001);
             oribitControls.update();
             this.renderer.render(this.scene, this.camera);
+
+            stats.update();
+
+            requestAnimationFrame(looper);
         }
         looper();
 
